58eb704a5932af7f6159019902e7ba5fb7e04182
Ethan Cairns <ecairns22@gmail.com>
2015-07-23 15:03:39 -0500

    Rewrite password service to return response, but the results, and allow the controller to do the response

--- api/controllers/PasswordController.js
+++ api/controllers/PasswordController.js
@@ -9,27 +9,65 @@ var createSendToken = require('../services/createSendToken.js');
 var emailVerification = require('../services/emailVerification.js');
 var password = require('../services/password.js');
 
+module.exports = {
+
+    /** Forgot Password **/
+    forgot: Forgot,
+
+    /** Reset a users password **/
+    reset: Reset,
+
+    /** Validate the accounts email address. **/
+    verifyEmail: emailVerification.verify
+};
+
+/**
+ *  Forgot password implementation
+ *  @params req
+ *  @params res
+ */
+function Forgot(req, res) {
+    var username = req.body.username;
+
+    password.forgot(username,function(err, results) {
+        if (err) {
+            return res
+                    .status(401)
+                    .send({message: err});
+        } else {
+            return res
+                    .status(200)
+                    .send({message: results});
+        }
+    })
+}
+
+/**
+ *  Reset password implementation
+ *  @params req
+ *  @params res
+ */
+function Reset(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    var token = req.body.token;
+
+    password.reset(username, password, token, function(err, results) {
+        if (err) {
+            return res
+                    .status(401)
+                    .send({message: err});
+        } else {
+            return res
+                    .status(200)
+                    .send({message: results});
+        }
+    });
+}
+
 /**
  * PasswordController
  *
  * @description :: Server-side logic for managing passwords
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
-
-module.exports = {
-
-    /**
-     * Forgot Password
-     */
-    forgot: password.forgot,
-
-    /**
-     * Reset a users password
-     */
-    reset: password.reset,
-
-    /**
-     * Validate the accounts email address.
-     */
-    verifyEmail: emailVerification.verify
-};
